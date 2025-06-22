f82d641fb49e9a3d984c6d59d77702426a745866
uyuyuy99 <uyuyuy99@gmail.com>
2025-06-21 21:44:07 -0700

    Initial commit

---
+++ src/main/java/me/uyuyuy99/punishments/Punishments.java
@@ -0,0 +1,17 @@
+package me.uyuyuy99.punishments;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Punishments extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
